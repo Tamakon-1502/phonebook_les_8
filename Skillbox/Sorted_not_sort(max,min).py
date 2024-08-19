d8751fe9f73eae89e90268b93696f2b9323ab891
Tamakon-1502 <150246144+Tamakon-1502@users.noreply.github.com>
2024-08-19 15:15:15 +0300

    add homework_15

---
+++ Skillbox/Sorted_not_sort(max,min).py
@@ -0,0 +1,22 @@
+# Задание 8. Сортировка. Без функции sorted/min/max и метод sort
+
+# start_list = [1, 4, -3, 0, 10]
+# sort_list = []
+# for i in range(len(start_list)):
+#     for curr in range(i, len(start_list)):
+#         if start_list[curr] < start_list[i]:
+#             start_list[curr], start_list[i] = start_list[i], start_list[curr]
+#     sort_list.append(start_list[i])
+# #print(start_list)
+# print(sort_list)
+
+# def selection(my_list):
+#     for i in range(len(my_list)):
+#         for curr in range(i, len(my_list)):
+#             if my_list[curr] < my_list[i]:
+#                 my_list[curr], my_list[i] = my_list[i], my_list[curr]
+#
+# numbers = [1, 4, -3, 0, 10]
+# print('Изначальный список:', numbers)
+# selection(numbers)
+# print('Отсортированый список', numbers)
